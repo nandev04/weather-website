6c8c2304a6d1249a93fb2c0e84bd0a9e2f7a2572
Renan Alves <renanlvsdv@gmail.com>
2024-01-12 18:57:36 -0300

    Iniciado o fetch da API, realizando algumas alterações no projeto, feito algumas conversões necessárias de medidas para o padrão brasileiro

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,60 +1,157 @@
 import React from 'react';
 import style from '../styles/modules/Main.module.css';
-import predoNublado from '../assets/predominantemente-nublado.svg';
+import logo from '../assets/logo.png';
 
 const Main = () => {
+  const [temperature, setTemperature] = React.useState('');
+  const [minTemperature, setMinTemperature] = React.useState('');
+  const [maxTemperature, setMaxTemperature] = React.useState('');
+  const [pressure, setPressure] = React.useState('');
+  const [humidity, setHumidity] = React.useState('');
+  const [feelLike, setFeelLike] = React.useState('');
+  const [wind, setWind] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [city, setCity] = React.useState('');
+  const [monthName, setMonthName] = React.useState('');
+  const [lat, setLat] = React.useState(null);
+  const [long, setLong] = React.useState(null);
+
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const monthNumber = currentDate.getMonth() + 1;
+  const year = currentDate.getFullYear();
+
+  function currentLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+    }
+  }
+
+  function convertNumberToName() {
+    switch (monthNumber) {
+      case 1:
+        setMonthName('Janeiro');
+        break;
+      case 2:
+        setMonthName('Fevereiro');
+        break;
+      case 3:
+        setMonthName('Março');
+        break;
+      case 4:
+        setMonthName('Abril');
+        break;
+      case 5:
+        setMonthName('Maio');
+        break;
+      case 6:
+        setMonthName('Junho');
+        break;
+      case 7:
+        setMonthName('Julho');
+        break;
+      case 8:
+        setMonthName('Agosto');
+        break;
+      case 9:
+        setMonthName('Setembro');
+        break;
+      case 10:
+        setMonthName('Outubro');
+        break;
+      case 11:
+        setMonthName('Novembro');
+        break;
+      case 12:
+        setMonthName('Dezembro');
+        break;
+    }
+  }
+
+  async function fetchWeather() {
+    const response = await fetch(
+      lat && long
+        ? `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=cb583e83caade67cb06d84f96762d184&units=metric&exclude=current&lang=pt_br`
+        : 'https://api.openweathermap.org/data/2.5/weather?q=Londres&appid=cb583e83caade67cb06d84f96762d184&units=metric&exclude=current&lang=pt_br',
+    );
+    const json = await response.json();
+    setTemperature(Math.trunc(json.main.temp));
+    setMinTemperature(Math.trunc(json.main.temp_min));
+    setMaxTemperature(Math.trunc(json.main.temp_max));
+    setPressure(json.main.pressure);
+    setHumidity(json.main.humidity);
+    setFeelLike(Math.trunc(json.main.feels_like));
+    setWind(json.wind.speed);
+    setDescription(
+      `${json.weather[0].description
+        .charAt(0)
+        .toUpperCase()}${json.weather[0].description.substring(1)}`,
+    );
+    setCity(json.name);
+  }
+
+  React.useEffect(() => {
+    currentLocation();
+    convertNumberToName();
+    fetchWeather();
+  }, []);
+
   return (
     <div className={style.container}>
       <div className={style.grid}>
         <main className={style.info}>
           <input type="text" placeholder="Pesquise sua cidade" />
-          <img
-            src={predoNublado}
-            alt="Imagem representando um clima predominantemente nublado"
-          />
-          <span className={style.temperature}>31°C</span>
-          <p className={style.generalInfo}>Mostly Cloudy</p>
+          <img src={logo} alt="Imagem representando o clima" />
+          <p className={style.temperature}>{temperature}°C</p>
+          <p className={style.generalInfo}>{description}</p>
           <div className={style.infoDay}>
-            <p className={style.date}>21-July-2023</p>
+            <p className={style.date}>
+              {day && day}-{monthName && monthName}-{year && year}
+            </p>
             <p>Friday, 12:44 PM</p>
             <p>Day</p>
           </div>
-          <h2 className={style.city}>Dhaka</h2>
+          <h2 className={style.city}>{city}</h2>
         </main>
         <section className={style.moreInfo}>
           <header className={style.navigationDay}>
             <ul>
               <li className={style.active}>Today</li>
-              <li>Tomorrow</li>
             </ul>
           </header>
           <div className={style.gridDetails}>
             <div>
               <h3>Wind</h3>
+              <p className={style.details}>
+                {Math.trunc(wind && wind * 3.6)} km/h
+              </p>
             </div>
             <div>
               <h3>Humidity</h3>
+              <p className={style.details}>{humidity && humidity}%</p>
             </div>
             <div>
               <h3>Real Feel</h3>
+              <p className={style.details}>{feelLike && feelLike}°C</p>
             </div>
             <div>
-              <h3>UV Index</h3>
+              <h3>Temperatura mínima</h3>
+              <p className={style.details}>
+                {minTemperature && minTemperature}°C
+              </p>
             </div>
             <div>
-              <h3>Pressure</h3>
+              <h3>Temperatura máxima</h3>
+              <p className={style.details}>
+                {maxTemperature && maxTemperature}°C
+              </p>
             </div>
             <div>
-              <h3>Chance of rain</h3>
-            </div>
-            <div>
-              <h3>Temperature History</h3>
-            </div>
-            <div>
-              <h3>Sun</h3>
-            </div>
-            <div>
-              <h3>Moon</h3>
+              <h3>Pressão</h3>
+              <p className={style.details}>{pressure && pressure} hPa</p>
             </div>
           </div>
           <footer>Todos os dados fornecidos pelo OpenWeather</footer>

caf1ac03d8c82285cf0d92de34936f6b4452040d
Renan Alves <renanlvsdv@gmail.com>
2024-01-17 04:43:01 -0300

    alterado o nome do repositório

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/weather_website',
+  base: '/weather-website',
 });

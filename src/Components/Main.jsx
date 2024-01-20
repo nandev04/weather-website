1db7d1005d53bf9fd40835936dc3ec200ac31009
Renan Alves <renanlvsdv@gmail.com>
2024-01-19 22:56:28 -0300

    Criado um custom hook para puxar a data

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,13 +3,12 @@ import style from '../styles/modules/Main.module.css';
 import Details from './Details';
 import useCurrentLocationFetch from '../customHooks/useCurrentLocationFetch';
 import useCustomFetch from '../customHooks/useCustomFetch';
+import useDates from '../customHooks/useDates';
 
 const Main = () => {
-  const [monthName, setMonthName] = React.useState('');
+  // State fetch
   const [lat, setLat] = React.useState(null);
   const [long, setLong] = React.useState(null);
-
-  // States fetch
   const [address, setAddress] = React.useState(null);
 
   //Fetch hooks
@@ -17,75 +16,18 @@ const Main = () => {
     useCurrentLocationFetch(lat, long);
   const { request } = useCustomFetch();
 
-  // Get dates and hours
-  const [dayPeriod, setDayPeriod] = React.useState(null);
-  const currentDate = new Date();
-  const day = currentDate.getDate();
-  const monthNumber = currentDate.getMonth() + 1;
-  const year = currentDate.getFullYear();
-
-  const weekDay = [
-    'Domingo',
-    'Segunda-feira',
-    'Terça-feira',
-    'Quarta-feira',
-    'Quinta-feira',
-    'Sexta-feira',
-    'Sábado',
-  ];
-
-  const hour = currentDate.getHours();
-  const dayName = currentDate.getDay();
-  const minutes = currentDate.getMinutes();
-
-  function convertNumberToName() {
-    if (hour >= 6 && hour < 12) {
-      setDayPeriod('Manhã');
-    } else if (hour >= 12 && hour < 18) {
-      setDayPeriod('Tarde');
-    } else {
-      setDayPeriod('Noite');
-    }
-
-    switch (monthNumber) {
-      case 1:
-        setMonthName('Janeiro');
-        break;
-      case 2:
-        setMonthName('Fevereiro');
-        break;
-      case 3:
-        setMonthName('Março');
-        break;
-      case 4:
-        setMonthName('Abril');
-        break;
-      case 5:
-        setMonthName('Maio');
-        break;
-      case 6:
-        setMonthName('Junho');
-        break;
-      case 7:
-        setMonthName('Julho');
-        break;
-      case 8:
-        setMonthName('Agosto');
-        break;
-      case 9:
-        setMonthName('Setembro');
-        break;
-      case 10:
-        setMonthName('Outubro');
-        break;
-      case 11:
-        setMonthName('Novembro');
-        break;
-      case 12:
-        setMonthName('Dezembro');
-        break;
-    }
-  }
+  // Date hook
+  const {
+    requestDate,
+    monthName,
+    dayPeriod,
+    day,
+    hour,
+    year,
+    dayName,
+    minutes,
+    weekDay,
+  } = useDates();
 
   function getCurrentLocation() {
     if (navigator.geolocation) {
@@ -123,7 +65,7 @@ const Main = () => {
   }, [lat && long]);
 
   React.useEffect(() => {
-    convertNumberToName();
+    requestDate();
     getCurrentLocation();
   }, []);
 
